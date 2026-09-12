d3518ce9d22e989e9fb04c38207059d0ef7c2493
Vinny Alberto <45470897+vny-albt@users.noreply.github.com>
2026-09-12 18:27:27 -0300

    Disable password requirement in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // CONFIGURAÇÃO DE SENHA E SISTEMA
-const REQUER_SENHA = true;         // Mude para false para DESATIVAR a senha
+const REQUER_SENHA = false;         // Mude para false para DESATIVAR a senha
 const SENHA_CORRETA = "f05turno3"; // Senha de acesso[cite: 2]
